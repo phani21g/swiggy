6e133239cdba65375da42f9f3d7d31b511b250ef
phanikumar <gspksvas2000@gmail.com>
2024-01-21 22:49:38 +0530

    using hooks on events

--- src/components/body.js
+++ src/components/body.js
@@ -1,13 +1,23 @@
 import {restaurants} from "../assets/data"
+import { useState } from "react";
+
 const Body=()=>{
+    const [restaurantdata,setrestaurantdata]=useState(restaurants);
     return (<div id="body">
+        {console.log(restaurantdata)}
         <div id="topbar">
-        <div id="col-two">
+        <div id="col-two">  
             <h5>SORT BY:</h5>
             <div id="btns">
-            <button >rating</button>
-            <button >time</button>
-            <button >price</button>
+                <button onClick={()=>{
+                    const filteredlist=restaurantdata.filter((res)=>res.info.avgRating>4.2);
+                    setrestaurantdata(filteredlist);
+                }}>rating</button>
+                <button onClick={()=>{
+                    const filteredlist=restaurantdata.filter((res)=>res.info.sla.deliveryTime<26);
+                    setrestaurantdata(filteredlist);
+                }}>time</button>
+                <button >price</button>
             </div>
         </div>
         <div id="col-one">
@@ -15,7 +25,7 @@ const Body=()=>{
             <input type="button" value="search" id="search-btn"></input>    
             </div>
         </div>
-        <div id="catalog">{restaurants.map((res)=><Card data={res}/>)}</div>
+        <div id="catalog">{restaurantdata.map((res)=><Card data={res} key={res.info.id}/>)}</div>
     </div>)
 }
 const Card=(data)=>{
